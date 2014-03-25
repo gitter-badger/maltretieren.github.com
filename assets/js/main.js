c79093a4de273ba7526224cde7e9dc4aba6e7a74
MalteAhrens <MalteAhrens@arcor.de>
2014-03-25 08:13:38 +0100

    test

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,20 +1,22 @@
-require.config({
-    baseUrl: '/assets/js',
-    paths: {
-        Mustache: '/assets/js/mustache',
-        Form: '/assets/js/form',
-        Chevron: '/assets/js/chevron',
-        Github: '/assets/js/github/github',
-        Base64: '/assets/js/github/base64',
-        Underscore: '/assets/js/github/underscore-min',
-        GithubFunctions: '/assets/js/github/github-functions'
-    }
-});
+(function() var scripts = {
+	Mustache: '/assets/js/mustache',
+	Form: '/assets/js/form',
+	Chevron: '/assets/js/chevron',
+	Github: '/assets/js/github/github',
+	Base64: '/assets/js/github/base64',
+	Underscore: '/assets/js/github/underscore-min',
+	GithubFunctions: '/assets/js/github/github-functions'
+}
 
-require(['Form'], function () {
+// Very simple asynchronous script-loader
 
-});
-
-require(['Github', 'Base64', 'Underscore', 'GithubFunctions'], function () {
-
+for (var i=0; i<scripts.length; i++) 
+	// Create a new script element
+	var script      = document.createElement('script');
+	// Find an existing script element on the page (usually the one this code is in)
+	var firstScript = document.getElementsByTagName('script')[0];
+	// Set the location of the script
+	script.src      = scripts[i];
+	// Inject with insertBefore to avoid appendChild errors
+	firstScript.parentNode.insertBefore( script, firstScript );
 });
