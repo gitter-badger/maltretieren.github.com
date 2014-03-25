a2b709dad66af5e064a020e2dd9ba1614479a580
Malteser <MalteAhrens@arcor.de>
2014-03-25 15:34:22 +0100

    test

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,29 +1,57 @@
-(function() { 
-	var scripts = {
-		Jquery: 'http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js',
-		Mustache: '/assets/js/mustache.js',
-		Form: '/assets/js/form.js',
-		Chevron: '/assets/js/chevron.js',
-		Base: '/assets/js/github/base64.js',
-		Underscore: '/assets/js/github/underscore-min.js',
-		Github: '/assets/js/github/github.js',
-		GithubFunctions: '/assets/js/github/github-functions.js',
-		BootstrapMarkdown: '/assets/js/bootstrapMarkdown/js/bootstrap-markdown.js'
-	};
+// HOWTO: load LABjs itself dynamically!
+// inline this code in your page to load LABjs itself dynamically, if you're so inclined.
 
-	// Very simple asynchronous script-loader
+(function (global, oDOC, handler) {
+    var head = oDOC.head || oDOC.getElementsByTagName("head");
 
-	for (var prop in scripts) {
-		if (scripts.hasOwnProperty(prop)) {
-			// Create a new script element
-			var script      = document.createElement('script');
-			// Find an existing script element on the page (usually the one this code is in)
-			var firstScript = document.getElementsByTagName('script')[0];
-			// Set the location of the script
-			script.src      = scripts[prop];
-			console.log("Script loaded: "+scripts[prop]) ;
-			// Inject with insertBefore to avoid appendChild errors
-			firstScript.parentNode.insertBefore( script, firstScript );
-		}
-	}
-})();
+    function LABjsLoaded() {
+         // Load the scripts. All scripts depend on jQuery. Then,
+		// only the cat-lover script depends on the friend.js to
+		// be loaded (so it can extend it).
+		//
+		// NOTE: We are storing a reference to the QUEUE so that
+		// we can refer to it later in the code.
+		var queue = $LAB
+		.script( "http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js" )
+		.wait()
+		.script( "/assets/js/mustache.js" )
+		.script( "/assets/js/chevron.js" )
+		.script( "/assets/js/github/base64.js" )
+		.wait()
+		.script( "/assets/js/github/github.js" )
+		.script( "/assets/js/github/github-functions.js" )
+		.script( "/assets/js/bootstrapMarkdown/js/bootstrap-markdown.js" )
+    }
+
+    // loading code borrowed directly from LABjs itself
+    setTimeout(function () {
+        if ("item" in head) { // check if ref is still a live node list
+            if (!head[0]) { // append_to node not yet ready
+                setTimeout(arguments.callee, 25);
+                return;
+            }
+            head = head[0]; // reassign from live node list ref to pure node ref -- avoids nasty IE bug where changes to DOM invalidate live node lists
+        }
+        var scriptElem = oDOC.createElement("script"),
+            scriptdone = false;
+        scriptElem.onload = scriptElem.onreadystatechange = function () {
+            if ((scriptElem.readyState && scriptElem.readyState !== "complete" && scriptElem.readyState !== "loaded") || scriptdone) {
+                return false;
+            }
+            scriptElem.onload = scriptElem.onreadystatechange = null;
+            scriptdone = true;
+            LABjsLoaded();
+        };
+        scriptElem.src = "/path/to/LAB.js";
+        head.insertBefore(scriptElem, head.firstChild);
+    }, 0);
+
+    // required: shim for FF <= 3.5 not having document.readyState
+    if (oDOC.readyState == null && oDOC.addEventListener) {
+        oDOC.readyState = "loading";
+        oDOC.addEventListener("DOMContentLoaded", handler = function () {
+            oDOC.removeEventListener("DOMContentLoaded", handler, false);
+            oDOC.readyState = "complete";
+        }, false);
+    }
+})(window, document);
