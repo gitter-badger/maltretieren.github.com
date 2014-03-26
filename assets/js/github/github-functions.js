df5e76cc0a1c9a08b601aa441defbae3db328b74
Malteser <MalteAhrens@arcor.de>
2014-03-26 14:37:46 +0100

    test

--- assets/js/github/github-functions.js
+++ assets/js/github/github-functions.js
@@ -54,7 +54,6 @@ var urlParams;
 (function() {
 	var editEnabled = urlParams['edit'];
 	if(typeof editEnabled != 'undefined') {
-		$("#content").markdown({'data-iconlibrary':'fa','savable':true})
 		var editorContent = getMarkup();
 	}
 })();
