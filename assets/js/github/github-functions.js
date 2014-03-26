9f7d0678bb8d7ad79fc4caabb0fcf8a69f4d02d3
Malteser <MalteAhrens@arcor.de>
2014-03-26 16:09:45 +0100

    test

--- assets/js/github/github-functions.js
+++ assets/js/github/github-functions.js
@@ -57,7 +57,7 @@ var urlParams;
 		var editorContent = getMarkup();
 		$('#target-editor').markdown({
 			savable:true,
-			'data-iconlibrary':"fa"
+			iconlibrary:"fa"
 		});
 		$('#target-editor').show();
 	}
