1e62b70fe8a471b588136d93a0227db0f51832ff
Crunsh <flamed@gmx.de>
2014-04-01 19:07:16 +0200

    comments moved to angularjs

--- assets/js/angular-app/controllers.js
+++ assets/js/angular-app/controllers.js
@@ -49,18 +49,27 @@ myApp.controller("ModalInstanceCtrl",function ($scope, $modalInstance, items) {
 });
 // you may add more controllers below
 
-myApp.controller("CommentsCtrl",function ($scope) {
-	var $scope.entries = {};
-	var url = "https://api.keen.io/3.0/projects/532b3e5a00111c0da1000006/queries/extraction?api_key=fca64cb411fe523d053f2d9b1d159011135be6ce55da682f1ad8d6b1d4f629b84dd564edb1c0d7a0d7575ebaaa79b55daa075f7c866d7430ace403bab51b7513aa41b30ce443f9d736d45d33c78a0b44420c2ecd35223b76d67af37df1d0cc52bf67e73cb32d949eb58cb5814e7e5e6a&event_collection=comments&timezone=3600"
-	var success = function (data) {			
-		$scope.comments = {
-			entries: data.result
-		}
-	};
+/**
+ * Receive a complete list of all comments
+ */
+myApp.controller("CommentsCtrl",function ($scope, $http) {
 
-	$.ajax({
-		url: url,
-		success: success
-	});
+	var commentsUrl = "https://api.keen.io/3.0/projects/532b3e5a00111c0da1000006/queries/extraction?api_key=fca64cb411fe523d053f2d9b1d159011135be6ce55da682f1ad8d6b1d4f629b84dd564edb1c0d7a0d7575ebaaa79b55daa075f7c866d7430ace403bab51b7513aa41b30ce443f9d736d45d33c78a0b44420c2ecd35223b76d67af37df1d0cc52bf67e73cb32d949eb58cb5814e7e5e6a&event_collection=comments&timezone=3600"
+
+    $http({method: 'GET', url: commentsUrl}).
+        success(function(data, status, headers, config) {
+            // this callback will be called asynchronously
+            // when the response is available
+            console.log("Successfully received comments from keen.io")
+            $scope.comments = {
+                entries: data.result
+            }
+        }).
+        error(function(data, status, headers, config) {
+            alert("Error while getting comments from keen.io: "+status)
+        });
+
+    $scope.getPartial = function () {
+        return '/assets/templates/comments.html';
+    }
 });
-// you may add more controllers below
--- assets/js/form.js
+++ assets/js/form.js
@@ -17,41 +17,8 @@
         commentForm("comments", data);
     };
 
-    var getComments = function() {
-        var url = "https://api.keen.io/3.0/projects/532b3e5a00111c0da1000006/queries/extraction?api_key=fca64cb411fe523d053f2d9b1d159011135be6ce55da682f1ad8d6b1d4f629b84dd564edb1c0d7a0d7575ebaaa79b55daa075f7c866d7430ace403bab51b7513aa41b30ce443f9d736d45d33c78a0b44420c2ecd35223b76d67af37df1d0cc52bf67e73cb32d949eb58cb5814e7e5e6a&event_collection=comments&timezone=3600"
-        var success = function (data) {			
-            $.get('/assets/templates/comments.mustache', function(template) {
-                // Filter array to match current page title, on frontpage show everything
-                var title = document.title;
-                // n is the current element, i the index
-                var result = data;
-
-                // a little hacky way to know if it is the frontpage
-                var parts = window.location.href.split("/");
-                if(parts.length != 4) {
-                    var resultSet = $.grep(data.result, function (element) {
-                        return (element.pageTitle === title);
-                    });
-                    result = {result:resultSet};
-                };
-
-                var output = Mustache.render(template, result);
-                $('#comments').append(output);
-            });
-                //$("#comments").append("<div class='comment'><div class='commentheader'><div class='commentgravatar'>" + '<img src="" alt="" width="20" height="20">' + "</div><a class='commentuser' href=''></a><a class='commentdate' href=''></a></div><div class='commentbody'>" + message + "</div></div>");
-
-        };
-
-        $.ajax({
-            url: url,
-            success: success
-        });
-    }
-
     var options = {
         valueNames: ['date', 'title', 'tags']
     };
 
     var entryList = new List('entry-list', options);
-
-    getComments();
