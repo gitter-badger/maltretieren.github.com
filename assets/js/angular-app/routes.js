837393064fbe8c7d4c5dbefea54684e6f38065e6
Malteser <MalteAhrens@arcor.de>
2014-04-01 09:20:10 +0200

    test

--- assets/js/angular-app/routes.js
+++ assets/js/angular-app/routes.js
@@ -4,13 +4,13 @@ var myApp = angular.module('myApp', ['ngRoute', 'ui.bootstrap']);
 
 myApp.config(function($routeProvider) {
     $routeProvider.when(
-    	'#/view1', 
+    	'/view1', 
     	{
     		templateUrl: '/assets/partials/partial1.html',
     		controller: 'MyCtrl1'
     	});
     $routeProvider.when(
-    	'#/view2', 
+    	'/view2', 
     	{
     		templateUrl: '/assets/partials/partial2.html',
     		controller: 'MyCtrl2'
