b92d4fca375c41a4c89e4aa8e98f3debbedf62c5
Malteser <MalteAhrens@arcor.de>
2014-04-01 09:05:32 +0200

    test

--- assets/js/angular-app/controllers.js
+++ assets/js/angular-app/controllers.js
@@ -9,4 +9,42 @@ myApp.controller("MyCtrl2" ,function ($scope) {
 
 });
 
+myApp.controller("ModalDemoCtrl" , $scope, $modal, $log) {
+  $scope.items = ['item1', 'item2', 'item3'];
+
+  $scope.open = function () {
+
+    var modalInstance = $modal.open({
+      templateUrl: '/assets/modal.html',
+      controller: ModalInstanceCtrl,
+      resolve: {
+        items: function () {
+          return $scope.items;
+        }
+      }
+    });
+
+    modalInstance.result.then(function (selectedItem) {
+      $scope.selected = selectedItem;
+    }, function () {
+      $log.info('Modal dismissed at: ' + new Date());
+    });
+  };
+});
+
+myApp.controller("ModalInstanceCtrl" , $scope, $modalInstance, items {
+  $scope.items = items;
+  $scope.selected = {
+    item: $scope.items[0]
+  };
+
+  $scope.ok = function () {
+    $modalInstance.close($scope.selected.item);
+  };
+
+  $scope.cancel = function () {
+    $modalInstance.dismiss('cancel');
+  };
+});
+
 // you may add more controllers below
