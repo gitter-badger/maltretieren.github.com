17c49de8f66cd7717e77b6300e09fa73c467d24b
Malteser <MalteAhrens@arcor.de>
2014-04-02 13:57:52 +0200

    moved

--- app/js/main.js
+++ app/js/main.js
@@ -23,7 +23,7 @@
 		.script( "/assets/js/bootstrap-markdown-2.3.1/libs/markdown.js" )
 		.script( "/assets/js/bootstrap-markdown-2.3.1/js/bootstrap-markdown.js" )
         .script ( "/assets/js/wikiquotes/wikiquote-api.js" )
-		.script( "/assets/js/github-0.9.0/github-functions.js" )
+		.script( "/app/js/github-functions.js" )
 		.script( "/assets/js/listjs-0.2.0/list.js" )
 		.script( "/app/js/form.js" )
         .script( "/assets/js/angular-1.3.0/angular.min.js" )
