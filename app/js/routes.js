59556aa5f9dc89a42c06a57e57bc92c80966dbff
Malteser <MalteAhrens@arcor.de>
2014-04-03 16:16:15 +0200

    test

--- app/js/routes.js
+++ app/js/routes.js
@@ -3,9 +3,7 @@
 var myApp = angular.module('myApp', ['ngRoute', 'ui.bootstrap']);
 
 myApp.config(function ($routeProvider, $locationProvider) {
-    // turning on html5Mode to have access to the parameters of the url
-	// see also: http://johan.driessen.se/posts/Manipulating-history-with-the-HTML5-History-API-and-AngularJS
-	$locationProvider.html5Mode(true);
+
 	$routeProvider.when(
     	'/view1', 
     	{
@@ -22,4 +20,7 @@ myApp.config(function ($routeProvider, $locationProvider) {
         {
             redirectTo: '/view1'
         });
+	    // turning on html5Mode to have access to the parameters of the url
+	// see also: http://johan.driessen.se/posts/Manipulating-history-with-the-HTML5-History-API-and-AngularJS
+	//$locationProvider.html5Mode(true);
 }]);
