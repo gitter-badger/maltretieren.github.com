83f56128bb29473a27561d9f39cab8dbead541e6
Malteser <MalteAhrens@arcor.de>
2014-04-03 11:22:20 +0200

    ets

--- app/js/services.js
+++ app/js/services.js
@@ -93,7 +93,7 @@ myApp.service("GithubSrvc", function (GithubUserService, GithubAuthService) {
 });
 
 // Inspired by http://joelhooks.com/blog/2013/04/24/modeling-data-and-state-in-your-angularjs-application/
-myApp.service("UserModel", function ($rootScope, GithubUserService) {
+myApp.service("UserModel", function ($rootScope) {
 	this.user = {}
 	this.loggedIn = false;
 	this.login = function(userName) {
