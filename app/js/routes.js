97801f16fd0a0fe1216d39f81d3d2aaa136e7db9
Malteser <MalteAhrens@arcor.de>
2014-04-03 15:49:04 +0200

    test

--- app/js/routes.js
+++ app/js/routes.js
@@ -1,7 +1,7 @@
 'use strict';
 
 
-var myApp = angular.module('myApp', ['ngRoute', 'ui.bootstrap', '$locationProvider']);
+var myApp = angular.module('myApp', ['ngRoute', 'ui.bootstrap']);
 
 
 myApp.config(function($routeProvider, $locationProvider) {
