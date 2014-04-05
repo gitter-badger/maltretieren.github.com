9a33462724d4aee50af8495e4337bd16e216a34d
Crunsh <flamed@gmx.de>
2014-04-06 00:40:07 +0200

    test

--- app/js/services.js
+++ app/js/services.js
@@ -91,7 +91,7 @@ myApp.service("GithubSrvc", function (GithubAuthService, UserModel, ParameterSrv
 			} else if(typeof oauthCode != "undefined" && (oauthToken != 'undefined' || oauthToken != null)) {
 				console.log("Code provided, no Token, request token - Code: "+oauthCode)
                 var callback = function() {
-                    alert("callback");
+                    this.userInfo().user();
                 }
                 GithubAuthService.requestToken(oauthCode, callback);
 			} else {
