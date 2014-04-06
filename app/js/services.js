e1440026faf50835c8d4d41925dac5cd2d07ab83
Crunsh <flamed@gmx.de>
2014-04-06 13:48:47 +0200

    test

--- app/js/services.js
+++ app/js/services.js
@@ -18,7 +18,7 @@ myApp.service("GithubAuthService", function ($http, UserModel) {
             error = function() {
                 alert("error");
             }
-            return { success: success() }
+            return { success: success }
         },
 		instance : function() {
 			var github = null;
@@ -57,7 +57,7 @@ myApp.service("GithubAuthService", function ($http, UserModel) {
 		},
         requestToken: function(oauthCode, callback) {
             $http({method: 'GET', url: 'https://maltretieren.herokuapp.com/authenticate/'+oauthCode}).
-                success(self.success()).error(self.error)
+                success(self.success).error(self.error)
         },
 		isTokenValid: function(token) {
 			console.log("Test if the token is still valid...");
