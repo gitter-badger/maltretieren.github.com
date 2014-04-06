0858861ce6651baad33357155cb18d8c03104661
Crunsh <flamed@gmx.de>
2014-04-06 16:17:58 +0200

    test

--- app/js/services.js
+++ app/js/services.js
@@ -31,7 +31,7 @@ myApp.service("GithubAuthService", function ($http, UserModel) {
 		instance : function() {
 			var github = null;
 			var oauthToken = localStorage.getItem("oauthToken");
-            console.log(oauthToken);
+            console.log("tttt"+oauthToken);
 			if(oauthToken != "undefined" && oauthToken != null) {
 				console.log("oauthToken is available");
 				github = new Github({
