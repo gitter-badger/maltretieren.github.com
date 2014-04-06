5d01c664cd8530b5db3b0cdcd2e2bb94700121a0
Crunsh <flamed@gmx.de>
2014-04-06 14:54:14 +0200

    test

--- app/js/services.js
+++ app/js/services.js
@@ -180,3 +180,24 @@ myApp.service("UserModel", function ($rootScope) {
 		$rootScope.$broadcast('UserModel::userLoggedOut');
 	}
 });
+
+/**
+ This is a helper function
+ **/
+myApp.service("ParameterSrvc", function ($window) {
+    var urlParams;
+    ($window.onpopstate = function () {
+        var match,
+            pl     = /\+/g,  // Regex for replacing addition symbol with a space
+            search = /([^&=]+)=?([^&]*)/g,
+            decode = function (s) { return decodeURIComponent(s.replace(pl, " ")); },
+            query  = window.location.search.substring(1);
+
+        urlParams = {};
+        while (match = search.exec(query))
+            urlParams[decode(match[1])] = decode(match[2]);
+    })();
+    return {
+        urlParams: urlParams
+    }
+});
