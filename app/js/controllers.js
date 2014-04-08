284a6ca09f7223c71f499259e9b84b077fd54598
Malteser <MalteAhrens@arcor.de>
2014-04-08 14:38:05 +0200

    test

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -119,7 +119,7 @@ myApp.controller("GithubCtrl", function ($scope, $location, $http, UserModel, Gi
     });
 });
 
-myApp.controller('ConfigCtrl', function($scope, $http, GithubSrvc, ToasterService) {
+myApp.controller('ConfigCtrl', function($scope, $http, GithubSrvc) {
     $scope.inputs = {}
 	$http({method: 'GET', url: '/app/js/config.json'}).success(function(data, status, headers, config) {
 		$scope.inputs = data;
