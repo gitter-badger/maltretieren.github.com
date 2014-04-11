455e6b45333a51e68e11acdce505ee79b54a1114
Malteser <MalteAhrens@arcor.de>
2014-04-11 13:32:21 +0200

    test

--- app/js/services.js
+++ app/js/services.js
@@ -202,7 +202,7 @@ myApp.service("GithubSrvc", function ($rootScope, $q, $interval, GithubAuthServi
 			var forkName = forkName;
 			branch.createBranch(branchName).done(function() {
 				console.log("master branch created from template branch");
-				that.deleteBranch(forkName, "heads/template"),
+				that.deleteBranch(forkName, "heads/template");
 			});
         },
 		commit: function(text, path) {
