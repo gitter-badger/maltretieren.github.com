fed8c0a604a159d492141d10859a12223fc96a43
Crunsh <flamed@gmx.de>
2014-04-12 21:04:34 +0200

    test

--- app/js/services.js
+++ app/js/services.js
@@ -117,6 +117,7 @@ myApp.service("GithubSrvc", function (
         },
 		fork: function(options) {
             // options contain the name for the new github page and the site slogan
+            console.log(options.forkSlogan);
 			var githubInstance = GithubAuthService.instance();
 			if(githubInstance != null) {
                 var repo = githubInstance.getRepo("Maltretieren", "maltretieren.github.com");
