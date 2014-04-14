9979fd0ad48acce54205ccd653e37ac939afef7b
Crunsh <flamed@gmx.de>
2014-04-14 21:31:48 +0200

    test

--- app/js/services.js
+++ app/js/services.js
@@ -315,10 +315,10 @@ myApp.service("UtilSrvc", function () {
 
 myApp.service("PollingSrvc", function ($q, $timeout, GithubAuthService) {
 
-
+    var deferred = $q.defer();
     var poll = function (repoName, branchName) {
         var resource = "README.md";
-        var deferred = $q.defer();
+
         // poll for availability - implement as promise, resolve as soon as it is available
         var githubInstance = GithubAuthService.instance();
         var repo = githubInstance.getRepo("flamed0011", repoName);
