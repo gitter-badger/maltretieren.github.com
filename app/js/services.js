03d46629aa3f74b62e1f62a66d57536c112f6ba3
Crunsh <flamed@gmx.de>
2014-04-16 21:23:59 +0200

    test

--- app/js/services.js
+++ app/js/services.js
@@ -242,6 +242,8 @@ myApp.service("GithubSrvc", function (
             var githubInstance = GithubAuthService.instance();
 			var repo = githubInstance.getRepo("Maltretieren", "maltretieren.github.com");
             var branch = repo.getBranch("master");
+            console.log(path);
+            console.log(text);
             branch.write(path, text, "save", false).done(function() {
                 console.log("saved");
             });
