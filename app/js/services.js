d962bad3231e043f2be20b45a011be0248f9d87b
Crunsh <flamed@gmx.de>
2014-04-16 17:12:05 +0200

    test

--- app/js/services.js
+++ app/js/services.js
@@ -347,7 +347,7 @@ myApp.service("PollingImgSrvc", function ($q, $timeout) {
                 var pollForImage = function() {
                     pollForImg();
                 }
-                $timeout(pollForImage,50000);
+                $timeout(pollForImage, 50000);
             }
             img.src = "https://flamed0011.github.com/app/img/ping.gif";
         }
