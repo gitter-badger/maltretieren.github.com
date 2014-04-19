c9c02bfe85e4a039731be7570e347efaf5dd17a5
Crunsh <flamed@gmx.de>
2014-04-19 11:15:44 +0200

    test

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -270,9 +270,9 @@ myApp.controller('GithubEditCtrl', function($scope, $modal, ParameterSrvc, Githu
     promise.then(function(content) {
         var commitPath = "";
         if($scope.options.date instanceof Date) {
-            commitPath = "_posts/"+$scope.options.date.toISOString().slice(0,10)+"-"+$scope.options.title.replaceAll(/ /g,"-")+".md";
+            commitPath = "_posts/"+$scope.options.date.toISOString().slice(0,10)+"-"+$scope.options.title.replace(/ /g,"-")+".md";
         } else {
-            commitPath = "_posts/"+$scope.options.date+"-"+$scope.options.title.replaceAll(/ /g,"-")+".md";
+            commitPath = "_posts/"+$scope.options.date+"-"+$scope.options.title.replace(/ /g,"-")+".md";
         }
 
         //var path = "_posts/"+$scope.options.date.toISOString().slice(0,10)+"-"+$scope.options.title.replaceAll(" ","-")+".md";
