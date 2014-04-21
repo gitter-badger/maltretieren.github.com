2f82aefc50c62ddce6057c83081839d179296479
Crunsh <flamed@gmx.de>
2014-04-21 03:06:47 +0200

    test

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -123,8 +123,8 @@ myApp.controller("GithubCtrl", function ($scope, $location, $http, UserModel, Gi
 myApp.controller('ConfigCtrl', function($scope, $http, $parse, GithubSrvc) {
     $scope.inputs = {}
 	$http({method: 'GET', url: '/app/js/config.js'}).success(function(data, status, headers, config) {
-		console.log($parse,(data));
-        data = $parse,(data);
+		console.log($parse(data));
+        data = $parse(data);
         $scope.inputs = data;
 	});
     $scope.setOutput = function(key, key2, newValue) {
