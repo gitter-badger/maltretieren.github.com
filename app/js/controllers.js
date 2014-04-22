8cf0ab380f86bdc97a050b695734fe95915c4afd
Crunsh <flamed@gmx.de>
2014-04-22 22:32:18 +0200

    test

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -219,6 +219,9 @@ myApp.controller('GithubForkCtrl', function($scope, $http, $q, toaster, GithubSr
     });
 
     $scope.fork = function() {
+        // predefined value
+        $scope.options.forkRealName = "Honey Bunny"; // this should be the user name
+
         var forkName = $scope.options.forkName;
         var forkSlogan = $scope.options.forkSlogan;
         var author = $scope.options.forkRealName;
