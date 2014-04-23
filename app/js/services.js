db0f11d59ab38c83fb6f723305a9e04d4266d010
Malteser <MalteAhrens@arcor.de>
2014-04-23 12:54:44 +0200

    test

--- app/js/services.js
+++ app/js/services.js
@@ -407,5 +407,16 @@ myApp.service("PollingImgSrvc", function ($q, $timeout) {
 myApp.service("StyleSwitcher", function () {
 	return { switch: function(styleName) {
 		console.log("switch to style: "+styleName);
+		var i, link_tag ;
+		  for (i = 0, link_tag = document.getElementsByTagName("link") ;
+			i < link_tag.length ; i++ ) {
+			if ((link_tag[i].rel.indexOf( "stylesheet" ) != -1) &&
+			  link_tag[i].title) {
+			  link_tag[i].disabled = true ;
+			  if (link_tag[i].title == css_title) {
+				link_tag[i].disabled = false ;
+			  }
+			}
+		  }
 	}}
 });
