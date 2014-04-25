b6fbb5b7d0d01983a4e9828c41060d0b25e694eb
Malteser <MalteAhrens@arcor.de>
2014-04-25 13:10:34 +0200

    test

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -88,7 +88,7 @@ myApp.controller("TableCtrl",function ($scope, $http) {
 /**
  * GitHub controller using the GitHub service
  */
-myApp.controller("GithubCtrl", function ($scope, $location, $http, $dialogs, $modalInstance, ParameterSrvc, UserModel, GithubSrvc, GithubAuthService) {	
+myApp.controller("GithubCtrl", ['$modalInstance', function ($scope, $location, $http, $dialogs, $modalInstance, ParameterSrvc, UserModel, GithubSrvc, GithubAuthService) {	
 	// login by the owner of the repository: edits on the blog are possible
 	// login by someone else: create an empty fork of the repository, automatically available
 	//      - ask for a name: the fork will be created for that name: xyz.github.io
@@ -171,7 +171,7 @@ myApp.controller("GithubCtrl", function ($scope, $location, $http, $dialogs, $mo
 		console.log("the GithubCtrl received an userLoggedOut event");
         $scope.user = "";
     });
-});
+}]);
 
 myApp.controller('ConfigCtrl', function($scope, GithubSrvc) {
     $scope.inputs = {}
