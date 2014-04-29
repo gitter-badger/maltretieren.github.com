9069f20e97959e7c40bad69c998849226367b141
Crunsh <flamed@gmx.de>
2014-04-29 19:46:29 +0200

    test

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -482,10 +482,10 @@ myApp.controller('ImportExportCtrl', function($scope, GithubSrvc) {
 		});
 	}
 
-    $scope.import = {}
+    $scope.import = {test: 'test'};
 	$scope.add = function(){
 	  console.log("read zip file");
-      $scope.import = {test: 'test'};
+      //$scope.import = {test: 'test'};
       console.log($scope.import);
 	  var f = document.getElementById('file').files[0],
 		  r = new FileReader();
