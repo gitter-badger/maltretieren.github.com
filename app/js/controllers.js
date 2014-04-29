f24a18cd0af3787c5469f473e0aa5a87497ea1de
Malteser <MalteAhrens@arcor.de>
2014-04-29 16:12:45 +0200

    test

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -481,6 +481,16 @@ myApp.controller('ImportExportCtrl', function($scope, GithubSrvc) {
 			saveAs(content, "example.zip");
 		});
 	}
+	
+	$scope.add = function(){
+	  var f = document.getElementById('file').files[0],
+		  r = new FileReader();
+	  r.onloadend = function(e){
+		var data = e.target.result;
+		//send you binary data via $http or $resource or do anything else with it
+	  }
+	  r.readAsBinaryString(f);
+	}
 });
 
 myApp.controller('GithubEditCtrl', function($scope, $dialogs, $modal, $timeout, UserModel, ParameterSrvc, GithubSrvc) {
