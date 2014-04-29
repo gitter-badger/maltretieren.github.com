32e029d96132c646cdb442a04da025af5553a1db
Malteser <MalteAhrens@arcor.de>
2014-04-29 14:23:51 +0200

    test

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -472,8 +472,6 @@ myApp.controller('ImportExportCtrl', function($scope) {
 		console.log("export posts...");
 		var zip = new JSZip();
 		zip.file("Hello.txt", "Hello World\n");
-		var img = zip.folder("images");
-		img.file("smile.gif", imgData, {base64: true});
 		var content = zip.generate({type:"blob"});
 		// see FileSaver.js
 		saveAs(content, "example.zip");
