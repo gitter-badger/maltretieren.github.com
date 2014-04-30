47c203c2746522dc3ee21c0e89b5146520754ba3
Malteser <MalteAhrens@arcor.de>
2014-04-30 09:50:16 +0200

    test

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -490,7 +490,9 @@ myApp.controller('ImportExportCtrl', function($scope, $dialogs, GithubSrvc) {
 			if($scope.maxValue === 0) {
 				$scope.maxValue = update;
 			} else {
-				$scope.exportStatus = ($scope.maxValue / 100) * update;
+				var percentage = ($scope.maxValue / 100) * update;
+				console.log("percentage: "+percentage);
+				$scope.exportStatus = percentage;
 			}
 		});
 	}
