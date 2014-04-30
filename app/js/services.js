e7a42428bcd7e5df4cbf6e145138aeef92e614fe
Malteser <MalteAhrens@arcor.de>
2014-04-30 16:24:26 +0200

    test

--- app/js/services.js
+++ app/js/services.js
@@ -255,7 +255,6 @@ myApp.service("GithubSrvc", function (
 				self.getContent(fileName).then(function(response) {
 					contents[fileName] = response.content;
 					if(i !== fileNames.length) {
-						$scope.processingPostNr
 						fileDeferred.notify(i);
 						doGet(fileNames[i]);
 						i++;
