4962effa3eb1c62785d75e38b7e8d1df98993910
Crunsh <flamed@gmx.de>
2014-05-04 18:55:46 +0200

    test

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -554,8 +554,8 @@ myApp.controller('ImportCtrl', function($scope, $dialogs, GithubSrvc) {
     }
 
     $scope.selectAllImport = function() {
-        console.log($scope.import);
-        //$scope.importSelection = angular.copy($scope.import);
+        //console.log($scope.import);
+        $scope.importSelection = angular.copy($scope.import);
         //console.log($scope.importSelection);
     }
     $scope.unselectAllImport = function() {
