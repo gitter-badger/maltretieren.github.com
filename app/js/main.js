118b5f1b507ba2ab6a657ef4c91fa41fc17b55b9
Malteser <MalteAhrens@arcor.de>
2014-05-06 14:30:17 +0200

    test

--- app/js/main.js
+++ app/js/main.js
@@ -24,7 +24,6 @@
 		.script( "/assets/js/bootstrap-markdown-2.3.1/js/bootstrap-markdown.js" )
         .script ( "/assets/js/wikiquotes/wikiquote-api.js" )
 		.script( "/assets/js/listjs-0.2.0/list.js" )
-		.script( "/app/js/form.js" )
         .script( "/assets/js/angular-1.3.0/angular.min.js" )
         .script( "/assets/js/angular-1.3.0/angular-route.min.js" )
         .script( "/assets/js/angular-1.3.0/angular-sanitize.min.js" )
