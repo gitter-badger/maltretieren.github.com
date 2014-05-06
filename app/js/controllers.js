8f0e223cc4bb275cdc8aa5673d9e746084d8cc92
Malteser <MalteAhrens@arcor.de>
2014-05-06 14:36:36 +0200

    test

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -50,11 +50,10 @@ myApp.controller("CommentsCtrl",function ($scope, $http, toaster) {
 		} 
 
 		var success = function() {
-			console.log("Succeessssfully sent comment");
 			toaster.pop('success', "Comment saved", '<ul><li>Comment was saved and will be available shortly</li></ul>', 5000, 'trustedHtml');
 		}
 		var error = function() {
-			console.log("Error sending comment");
+			toaster.pop('error', "Comment failed", '<ul><li>There was an error while saving the comment</li></ul>', 5000, 'trustedHtml');
 		}
 		Keen.addEvent("comments", data, success);
 	}
