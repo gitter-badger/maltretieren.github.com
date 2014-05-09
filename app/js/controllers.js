8fb447e2039298aeb438b238932ffb1809facb42
Malteser <MalteAhrens@arcor.de>
2014-05-09 15:40:59 +0200

    test

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -93,7 +93,7 @@ myApp.controller("CommentsCtrl",function ($scope, $http, $dialogs,$timeout, toas
 
 myApp.controller("KeenioMasterCtrl", function ($scope, $modalInstance, UserModel, GithubAuthService, GithubSrvc) {
 	$scope.user = {};
-	console.log($scope.commentId);
+	console.log(this);
 	
 	$scope.cancel = function(){
 		$modalInstance.dismiss('canceled');  
