a22ae7fa0d40a7f3052778d806175a31fea478c1
Malteser <MalteAhrens@arcor.de>
2014-05-12 15:57:21 +0200

    test

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -110,7 +110,8 @@ myApp.controller("KeenioMasterCtrl", function ($scope, $modalInstance, data) {
         console.log("Keenio Master key: "+$scope.masterKey);
 		console.log("Keenio Comment Id: "+$scope.commentId);
 		//$scope.$resolve
-		var PostsUrl = 'https://api.keen.io/3.0/projects/532b3e5a00111c0da1000006/events/comments?api_key='+masterKey+'&filters=[{"property_name":"keen.id","operator":"eq","property_value":"'+$scope.id+'"}]';
+		var postsUrl = 'https://api.keen.io/3.0/projects/532b3e5a00111c0da1000006/events/comments?api_key='+$scope.masterKey+'&filters=[{"property_name":"keen.id","operator":"eq","property_value":"'+$scope.id+'"}]';
+		console.log(postsurl)
 		$http({method: 'POST', url: postsUrl}).
 			success(function(data, status, headers, config) {
 				// this callback will be called asynchronously
