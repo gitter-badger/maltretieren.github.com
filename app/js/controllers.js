2f81a10b750ac7f9fc34bf01756dfda3e7299ea4
Malteser <MalteAhrens@arcor.de>
2014-05-14 07:55:50 +0200

    test

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -709,7 +709,7 @@ myApp.controller('GithubEditCtrl', function($scope, $dialogs, $q, $modal, $timeo
     var contentPromise = GithubSrvc.editContent(path);
 	contentPromise.then(function(yaml) {
 		// attach yaml object to scope
-		scope.yaml = yaml;
+		$scope.yaml = yaml;
 		$scope.$apply();
 	});
 	
