54b37540565c629423e40e335974b8716b10b170
Crunsh <flamed@gmx.de>
2014-05-14 20:37:36 +0200

    test

--- app/js/main.js
+++ app/js/main.js
@@ -27,6 +27,7 @@
 		.script( "/assets/js/angular-1.3.0/angular-animate.min.js" )
         .script( "/assets/js/angular-1.3.0/angular-sanitize.min.js" )
         .script( "/assets/js/dialogs-3.0/dialogs.min.js" )
+            .script( "/assets/js/bootstrap-tagsinput-0.3.9/bootstrap-tagsinput.min.js" )
         .script( "/assets/js/bootstrap-tagsinput-0.3.9/bootstrap-tagsinput-angular.js" )
 		.script( "/assets/js/angular-1.3.0/angular-animate.min.js" )
         .script( "/assets/js/angular-1.3.0/angular-resource.min.js" )
