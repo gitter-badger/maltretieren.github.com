271520e4dd37642435cc90768214649a9812a61b
Crunsh <flamed@gmx.de>
2014-05-14 19:50:26 +0200

    test

--- app/js/services.js
+++ app/js/services.js
@@ -528,11 +528,12 @@ myApp.service("YamlSrvc", function () {
                     response[line[0]] = value;
 				} else {
 					var element = line[0].split("-");
-                    var value = element[1].trim();
+                    var value = "";
 					if(element.length === 1) {
 						remember = value;
 						response[remember] = [];
 					} else {
+                        value = element[1].trim();
                         if(value === "true" || value === "false") {
                             value = Boolean(element[1].trim());
 						    response[remember].push(value);
