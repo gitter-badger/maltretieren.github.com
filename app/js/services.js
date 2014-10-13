58934eda0e015adbd761b6551c445032863fb396
crunsh <maltretieren.github.com>
2014-10-13 21:32:14 +0200

    afd

--- app/js/services.js
+++ app/js/services.js
@@ -504,7 +504,6 @@ myApp.service("PollingSrvc", function ($q, $timeout, UserModel, GithubAuthServic
 myApp.service("YamlSrvc", function () {
     var parse = function (content) {
 		var response = {};
-        console.log(content);
 
         // if there is a frontmatter available....
 		var contentSplit = content.split("---");
