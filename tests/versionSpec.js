6b4525af9ff45d746d8399d3b21d2e1aa5d6a410
crunsh <maltretieren.github.com>
2014-10-15 23:31:12 +0200

    test

--- tests/versionSpec.js
+++ tests/versionSpec.js
@@ -2,6 +2,6 @@ describe('version', function() {
     beforeEach(module('myApp.services'));
 
     it('should return current version', inject(function(version) {
-        expect(version).toEqual('0.2');
+        expect(version).toEqual('0.1');
     }));
 });
