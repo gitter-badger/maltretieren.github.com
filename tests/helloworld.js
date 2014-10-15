05cb505ca9ee79128bf95ca4c9450c46cb6be797
crunsh <maltretieren.github.com>
2014-10-15 19:31:34 +0200

    test

--- tests/helloworld.js
+++ tests/helloworld.js
@@ -1,2 +1,7 @@
-console.log('Hello, world!');
-phantom.exit();
+console.log('Loading a web page');
+var page = require('webpage').create();
+var url = 'http://www.phantomjs.org/';
+page.open(url, function (status) {
+    //Page is loaded!
+    phantom.exit();
+});
