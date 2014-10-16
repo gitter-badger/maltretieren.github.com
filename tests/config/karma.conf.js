b56a498cddb59d99d74b55a12bcea12d540b93e8
Malteser <MalteAhrens@arcor.de>
2014-10-16 08:56:42 +0200

    test

--- tests/config/karma.conf.js
+++ tests/config/karma.conf.js
@@ -2,9 +2,8 @@ module.exports = function(config){
     config.set({
         basePath : '../../',
         files : [
-            'assets/js/angular-1.3.0/angular.js',
-            'assets/js/angular-1.3.0/angular-mocks.js',
 			'assets/js/**/*.js',
+			'assets/js/angular-1.3.0/angular-mocks.js',
 			'app/js/main.js',
             'app/version.js',
             'tests/versionSpec.js'
